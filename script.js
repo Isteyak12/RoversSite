a1ea39bc5d39c7736954df792d7fd44defd49f32
isteyak <isteyakislam12@gmail.com>
2024-05-16 17:47:09 -0400

    hi

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    // Simulate loading delay
+    setTimeout(function() {
+      document.getElementById('loader').classList.add('hidden');
+      document.getElementById('content').classList.add('visible');
+    }, 2000); // Adjust the delay as needed
+  };
+  
